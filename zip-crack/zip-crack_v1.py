eb43583600f35f9dc6eea59fbe5fffc4e290eb8b
Sec-Soup <48111069+Sec-Soup@users.noreply.github.com>
2021-01-26 20:49:28 -0600

    Update zip-crack_v1.py

--- zip-crack/zip-crack_v1.py
+++ zip-crack/zip-crack_v1.py
@@ -3,7 +3,7 @@
 # The passwords.txt file that is a companion to this script is spefically designed for crimeware variants such as Valak, Ursnif, and Dridex that are often delivered as ZIP archives.
 # The only dependency the user should need is pyzipper. See this documentation to install: https://pypi.org/project/pyzipper/
 # Author: Ryan Campbell @sec_soup
-# Acknowledgments: Special thanks to @AbsoZed, becuase I borrowed some of his code. 
+# Acknowledgments: Special thanks to @HBRH_314, becuase I borrowed some of his code. 
 # See Disclaimer at end
 
 import argparse
